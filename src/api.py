274866c574d11c438707309701722b9057602144
Lucas Chen Alba <lchends@gmail.com>
2021-11-28 02:20:22 -0300

    Adding monsters data and minor text fixes

--- src/api.py
+++ src/api.py
@@ -33,14 +33,21 @@ artisan_data = json.load(artisan_data_file)
 gathering_data_file = open(os.path.dirname(__file__) + '/../gathering_data.json')
 gathering_data = json.load(gathering_data_file)
 
+monsters_data_file = open(os.path.dirname(__file__) + '/../monsters_data.json')
+monsters_data = json.load(monsters_data_file)
+
 @app.get("/exp", tags=["exp"])
-async def get_todos() -> dict:
+async def get_exp() -> dict:
     return exp_data
 
 @app.get("/artisan", tags=["artisan"])
-async def get_todos() -> dict:
+async def get_artisan() -> dict:
     return artisan_data
 
 @app.get("/gathering", tags=["gathering"])
-async def get_todos() -> dict:
+async def get_gathering() -> dict:
     return gathering_data
+
+@app.get("/monsters", tags=["monsters"])
+async def get_monsters() -> dict:
+    return monsters_data
